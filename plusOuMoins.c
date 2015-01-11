061df7233aefd016a704e189ca2e2c0dee7b9454
HimuraXp <larbi_yohann@hotmail.com>
2015-01-11 16:12:33 +0100

    Commentaires plusOuMoins.c

--- plusOuMoins.c
+++ plusOuMoins.c
@@ -4,19 +4,24 @@
 
 int main (int argc, char *argv)
 {
+	//Initialisation des variables.
 	int nombreMystere = 0, nombreChoix = 0;
 	const int MAX = 100, MIN = 1;
 	
+	//Création d'un nombre aléatoire compris entre 1 et 100.
 	srand(time(NULL));
 	nombreMystere = (rand() % (MAX -MIN + 1)) + MIN;
 		 
 	printf("Bienvenue sur Plus ou Moins !\nLe but de ce jeu est de deviner le Nombre Mystere.\nLe Nombre Mystere est compris entre 1 et 100 !\n");
-		
+	
+	//Tant que le nombre entré est different du nombre aléatoire on recommence. 	
 	do
 	{
+		//On demande le nombre.
 		printf("Quel est le nombre ? ");
 		scanf("%d", &nombreChoix);
 
+		//On compare le nombre entré avec le nombre aléatoire.
 		if (nombreMystere > nombreChoix)
 			{printf("C'est plus !\n");}
 
